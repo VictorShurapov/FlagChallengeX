9da873891549682427dc95435a575e23efc19240
Viktor <vshurapov@gmail.com>
2025-02-18 16:24:58 +0200

    Initial Commit

---
+++ FlagChallengeX/FlagChallengeXApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagChallengeXApp.swift
+//  FlagChallengeX
+//
+//  Created by Yaro4ka on 18.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct FlagChallengeXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
